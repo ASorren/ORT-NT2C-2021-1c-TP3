1e5217b07a68f34a6d502edd965143e802b51b1d
Agustin Sorrentino <aa.sorrentino94@gmail.com>
2021-04-11 19:26:47 -0300

    Cambio de nombre en class onClick

--- script.js
+++ script.js
@@ -5,11 +5,91 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
+// variables a usar
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
+const list = document.querySelector('#todo-list')
+const itemCountSpan = document.querySelector('#item-count')
+const uncheckedCountSpan = document.querySelector('#unchecked-count')
 
-function addTodo() {
-  alert('Boton Add TODO clickeado!')
+// se pone let y no const porque es variable (array donde acumulas tareas del prompt)
+let tareas = [];
+
+
+//clase que va al array
+
+class Tarea {
+  
+  // constructor de la clase (viene por parametro el titulo)
+  constructor(titulo){
+    this.titulo = titulo;
+    this.terminada = false;
+  }
+
 }
+
+function agregarTarea(){
+  const titulo = prompt('Ingrese tarea');
+  // se crea una tarea con el contenido de lo que pongas en el prompt
+  const tarea = new Tarea(titulo);
+
+  // se agrega al Array
+  tareas.push(tarea);
+
+  renderizarArray();
+
+}
+
+function renderizarArray(){
+
+  list.innerHTML = '';
+
+  tareas.forEach((tarea)=>{
+    // creo el checkbox de tarea cumplida o no
+    const cb = document.createElement('input')
+    // le doy atributos
+    cb.type = 'checkbox';
+    cb.checked = tarea.terminada;
+    cb.onchange = togglebox.bind(tarea);
+
+    // creo el span (donde va cada tarea);
+    const span = document.createElement('span');
+    span.className = classNames.TODO_TEXT;
+    span.textContent = tarea.titulo;
+
+      // creo la lista donde van a ir todos los span
+    const li = document.createElement('li');
+    li.className = classNames.TODO_ITEM;
+    
+        // le hago el append correspondiente de padre a hijos
+        li.appendChild(span);
+        li.appendChild(cb);
+        list.appendChild(li);
+
+          // Esto sirve para el contador de tareas que declaramos arriba 
+          // uno solamente va con el .lenght(ya que es un array)
+          // declaras un unchecjed y lo asignas a al span  y haces ++ si es no terminada
+       itemCountSpan.innerHTML = tareas.length
+       let unchecked = 0
+       tareas.forEach((t)=> {
+         if (!t.terminada)
+            unchecked++
+    })
+    uncheckedCountSpan.innerHTML = unchecked
+
+
+
+
+  } )
+
+  
+}
+
+function togglebox(eventito) {
+
+  this.terminada = eventito.target.checked
+  renderizar()
+
+}
+
+
+
